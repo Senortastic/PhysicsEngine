05b65e178f14c58811d4c98d38fa56d08889971f
Dean Mackay <Deanmackayn7@gmail.com>
2016-08-22 00:18:44 +1000

    Change Update to return vec3 and return positionDelta

--- CustomPhysicsForGames/PhysicsForGames/source/RigidBody.cpp
+++ CustomPhysicsForGames/PhysicsForGames/source/RigidBody.cpp
@@ -1,12 +1,13 @@
 #include "RigidBody.h"
 
-void RigidBody::Update(float deltaTime, vec3 gravity)
+vec3 RigidBody::Update(float deltaTime, vec3 gravity)
 {
 	vec3 acceleration = force / mass;
 	vec3 oldVelocity = velocity;
 	velocity += acceleration * deltaTime;
 	velocity += gravity * deltaTime;
 
-	positionDelta = (oldVelocity + velocity) * 0.5f * deltaTime;
+	glm::vec3 positionDelta = (oldVelocity + velocity) * 0.5f * deltaTime;
 	force = vec3(0);
+	return positionDelta;
 }
--- CustomPhysicsForGames/PhysicsForGames/source/RigidBody.h
+++ CustomPhysicsForGames/PhysicsForGames/source/RigidBody.h
@@ -7,18 +7,16 @@ class RigidBody
 {
 public:
 	RigidBody(float mass, vec3 initialVelocity) : mass(mass), velocity(initialVelocity){}
-	void Update(float deltaTime, vec3 gravity);
+	vec3 Update(float deltaTime, vec3 gravity);
 	float GetMass() { return mass; }
 	vec3 GetVelocity() { return velocity; }
 	vec3 GetMomentum() { return GetMass() * GetVelocity(); }
-	vec3 GetPositionDelta() { return positionDelta; }
 	void Stop() { velocity = vec3(0); }
-	void AddVelocity(vec3 velocity) { velocity += velocity; }
-	void AddMomentum(vec3 momentum) { AddVelocity(momentum / mass); }
-	void AddForce(vec3 force) { force += force; }
+	void AddVelocity(vec3 velocity) { this->velocity += velocity; }
+	void AddMomentum(vec3 momentum) { AddVelocity( momentum / mass); }
+	void AddForce(vec3 force) { this->force += force; }
 private:
 	float mass;
 	vec3 velocity;
 	vec3 force;
-	vec3 positionDelta;
 };
