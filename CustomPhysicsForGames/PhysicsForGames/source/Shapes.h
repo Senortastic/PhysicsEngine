ac1b3c86cf292e466db307152096dead6a105a76
Dean Mackay <Deanmackayn7@gmail.com>
2016-08-21 22:49:44 +1000

    Change variables

--- CustomPhysicsForGames/PhysicsForGames/source/Shapes.h
+++ CustomPhysicsForGames/PhysicsForGames/source/Shapes.h
@@ -6,7 +6,7 @@ using namespace glm;
 class Shape
 {
 public:
-	int GetShapeType() { return static_cast<int>(m_id); }
+	int GetShapeType() { return static_cast<int>(id); }
 	static int GetShapeCount() { return static_cast<int>(ShapeType::Count); }
 	virtual void Draw(vec3 position) const = 0;
 protected:
@@ -17,49 +17,49 @@ protected:
 		AABB,
 		Count,
 	};
-	Shape(ShapeType id) : m_id(id) {}
+	Shape(ShapeType id) : id(id) {}
 private:
-	ShapeType m_id;
+	ShapeType id;
+	vec3 colour;
 };
 
 class AABB : public Shape
 {
 public:
-	//TODO: overload for constructor and store colour
-	AABB(vec3 size) : Shape(ShapeType::AABB), m_size(size) {}
-	vec3 GetSize() const { return m_size; }
+	AABB(vec3 size) : Shape(ShapeType::AABB), size(size) {}
+	vec3 GetSize() const { return size; }
 	void Draw(vec3 position) const override
 	{
-		Gizmos::addAABBFilled(position, m_size, vec4(0, 0.5, 0, 1));
+		Gizmos::addAABBFilled(position, size, vec4(0.5, 0, 0.5, 1));
 	}
 private:
-	vec3 m_size;
+	vec3 size;
 };
 
 class Plane : public Shape
 {
 public:
-	Plane(vec3 normal, float offset) : Shape(ShapeType::Plane), m_normal(normal), m_offset(offset) {}
-	vec3 GetNormal() const { return m_normal; }
-	float GetDistance() const { return m_offset; }
+	Plane(vec3 normal, float offset) : Shape(ShapeType::Plane), normal(normal), offset(offset) {}
+	vec3 GetNormal() const { return normal; }
+	float GetDistance() const { return offset; }
 	void Draw(vec3 position) const override
 	{
-		Gizmos::addAABBFilled(vec3(0, m_offset, 0), vec3(100.f, 0.01f, 100.f), vec4(0.25f, 0.25f, 0.25f, 1));
+		Gizmos::addAABBFilled(vec3(0, offset, 0), vec3(100.f, 0.01f, 100.f), vec4(0.25f, 0.25f, 0.25f, 1));
 	}
 private:
-	vec3 m_normal;
-	float m_offset; //distance
+	vec3 normal;
+	float offset;
 };
 
 class Sphere : public Shape
 {
 public:
-	Sphere(float radius) : Shape(ShapeType::Sphere), m_radius(radius){}
-	float GetRadius() const { return m_radius; }
+	Sphere(float radius) : Shape(ShapeType::Sphere), radius(radius){}
+	float GetRadius() const { return radius; }
 	void Draw(vec3 position) const override
 	{
-		Gizmos::addSphereFilled(position, m_radius, 10, 10, vec4(0.5f, 0, 0, 1));
+		Gizmos::addSphereFilled(position, radius, 10, 10, vec4(0.5f, 0, 0, 1));
 	}
 private:
-	float m_radius;
+	float radius;
 };
